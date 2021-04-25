da1734f16fa507652d111ca273470aa42f4acd97
SaraLealLima <saral.limasm@gmail.com>
2021-04-25 17:42:49 -0300

    Components Part 1

---
+++ src/app/shared/wine.ts
@@ -0,0 +1,11 @@
+export class Wine {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    country: string;
+    description: string;
+}
